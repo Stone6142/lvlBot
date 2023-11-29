523f3dace552a569c8d887751e9b71569da88149
Sky B <120291635+Stone6142@users.noreply.github.com>
2023-11-29 17:00:09 -0600

    Update bizno.js

--- commands/bizno.js
+++ commands/bizno.js
@@ -5,13 +5,13 @@ module.exports = {
       const bizEmbed = new Discord.MessageEmbed()
               .setColor('RANDOM')
               .setTitle('biz details for ' + message.author.username)
-              .setURL('https://github.com/CKStudios2018/lvlBot/blob/main/stats.json')
+              .setURL('https://github.com/stone6142/lvlBot/blob/main/stats.json')
               .addFields(
                   {name: 'biz', value: userStats.own_store + ' stores', inline: true},
                   {name: 'Bonus Coin Multiplier', value: userStats.own_store, inline: true}
               )
               .addField('\u200b', '\u200b')
-              .addField("Help", '[Link](https://ckstudios2018.github.io/lvlBot/help/)')
+              .addField("Help", '[Link](https://stone6142.github.io/lvlBot/help/)')
               .setTimestamp()
               .setFooter(`biz details for ` + message.author.username + " (" + message.author.id + ")")
           message.channel.send(bizEmbed);
